219cf840d615244fc238576e3b2a70b51d6ee035
renatobertone <cassimiro.bertone@gmail.com>
2021-06-09 11:33:53 -0700

    Exercíco Final Aula 'Criando Objetos Com Construtores'

---
+++ src/main/java/desenvolvimento_basico_java/criando_obj_com_construtores/Carro.java
@@ -0,0 +1,45 @@
+package desenvolvimento_basico_java.criando_obj_com_construtores;
+
+public class Carro {
+
+    public Carro (String marca, String modelo,Integer ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.variante = "variante";
+    }
+
+    private String marca ;
+    private String modelo ;
+    private Integer ano ;
+    private String variante ;
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public Integer getAno(){
+        return ano;
+    }
+
+    public String getVariante(){
+        return variante;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
